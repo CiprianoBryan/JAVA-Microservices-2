11fa49dd9e05fe0a2568fac11ea4d76eb5826dfa
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-10-12 13:17:16 -0500

    API REST - Get - Post - Put - Delete

--- service-product/src/main/java/academy/arima/store/product/controller/ProductController.java
+++ service-product/src/main/java/academy/arima/store/product/controller/ProductController.java
@@ -1,13 +1,14 @@
 package academy.arima.store.product.controller;
 
+import academy.arima.store.product.entity.Category;
 import academy.arima.store.product.entity.Product;
 import academy.arima.store.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,11 +18,62 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<Product>> listProduct() {
-        List<Product> products = productService.listAllProduct();
-        if (products.isEmpty()) {
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<List<Product>> listProduct(@RequestParam(required = false) Long categoryId) {
+        List<Product> products = new ArrayList<>();
+        if (categoryId == null) {
+            products = productService.listAllProduct();
+            if (products.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+        } else {
+            products = productService.findByCategory(Category.builder().id(categoryId).build());
+            if (products.isEmpty()) {
+                return ResponseEntity.notFound().build();
+            }
         }
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        Product product = productService.getProduct(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product productCreate = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        product.setId(id);
+        Product productDB = productService.updateProduct(product);
+        if (productDB == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productDB);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable Long id) {
+        Product product = productService.deleteProduct(id);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}/stock")
+    public ResponseEntity<Product> updateStockProduct(@PathVariable Long id, @RequestParam Double quantity) {
+        Product product = productService.updateStock(id, quantity);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
     }
 }
