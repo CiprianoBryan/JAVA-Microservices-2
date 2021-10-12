00cd6fc5cb48928884c9c8e92477472e4866a740
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-10-12 15:54:02 -0500

    start customer service

---
+++ customer-service/src/test/java/academy/arima/store/customerservice/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.arima.store.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
