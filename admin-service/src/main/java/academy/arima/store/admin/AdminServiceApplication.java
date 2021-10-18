4cdc60ca300c6566a28934c88da5e7b850055581
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-10-17 23:43:27 -0500

    Start Admin Server (admin-service)

---
+++ admin-service/src/main/java/academy/arima/store/admin/AdminServiceApplication.java
@@ -0,0 +1,14 @@
+package academy.arima.store.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+
+}
